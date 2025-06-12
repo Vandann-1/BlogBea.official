df848cb7aac1dc97e4fe668d04f6602ce1e55483
Vandann-1 <vandanparmar6041@gmail.com>
2025-06-12 22:22:02 +0530

    ssss

--- blogweb/settings.py
+++ blogweb/settings.py
@@ -32,9 +32,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--v5t71sv6$z4j#fiot^y_4!_2ho@l%0!u+8ejmi)*0e=#38q$2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["https://blogbeat-1-sn2e.onrender.com"]
+ALLOWED_HOSTS = ["blogbeat-1-sn2e.onrender.com"]
 
 
 # spotify
