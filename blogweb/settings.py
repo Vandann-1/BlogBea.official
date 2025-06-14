d304a7c8b8e3fd61f7a6e5d70228b133de7608db
Vandann-1 <vandanparmar6041@gmail.com>
2025-06-14 17:31:05 +0530

    blog chnges

--- blogweb/settings.py
+++ blogweb/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = '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'
-DEBUG = False
+DEBUG = True
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,7 +29,7 @@ DEBUG = False
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure--v5t71sv6$z4j#fiot^y_4!_2ho@l%0!u+8ejmi)*0e=#38q$2'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 
 ALLOWED_HOSTS: ['127.0.0.1', 'localhost', 'blogbeat-1-sn2e.onrender.com']
 
